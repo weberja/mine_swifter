b54c69c3f35a11c8890ba7bed60156f05bd6f106
weberja <12197882+weberja@users.noreply.github.com>
2025-03-04 20:22:15 +0100

    fit to canvas for wasm

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ pub mod states;
 pub mod ui;
 pub mod utils;
 
+use std::default;
+
 use assets::startup_assets;
 use bevy::{asset::AssetMetaCheck, prelude::*};
 use rand_chacha::ChaCha8Rng;
@@ -17,10 +19,19 @@ use utils::random::RandomSource;
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(AssetPlugin {
-                meta_check: AssetMetaCheck::Never,
-                ..default()
-            }),
+            DefaultPlugins
+                .set(AssetPlugin {
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "MineSwifter".into(),
+                        fit_canvas_to_parent: true,
+                        ..default()
+                    }),
+                    ..default()
+                }),
             states,
             startup_assets,
             ui::init,
